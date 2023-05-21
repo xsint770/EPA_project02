f0e716787195ac5e824ab77fed71f6b7c015effa
Tereza S <lycosa@post.cz>
2023-05-21 21:40:31 +0200

    pravidla.py created

---
+++ pravidla.py
@@ -0,0 +1,18 @@
+pravidla = '''
+Hraci zvoli velikost hraciho pole, zadaji pocet radku. Hraci pole je 
+ctvercove se stejnym poctem radku a sloupcu. Pocet sloupcu se nezadava.
+Minimalni pocet radku je tri.
+
+Zacinajici hrac je zvolen losem.
+
+Hraci se stridaji v obsazovani policek hraciho pole tak, ze zadaji cislo
+policka. Jako napoveda muze poslouzit planek hraciho pole. 
+
+Vitezi ten, kdo jako prvni obsadi tri sousedni policka, a to ve smeru
+horizontalnim, vertikalnim, nebo v diagonale.
+
+Pokud hrac vybere obsazene policko ztraci pro aktualni kolo moznost
+volby a hraje souper.
+
+Hru lze kdykoli ukoncit zadanim Q namisto cisla policka.
+'''
